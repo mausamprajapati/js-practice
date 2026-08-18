feffd8064ee564dc220f8abef0b30c0bf7240074
Mausam Prajapati <230212408+mausamprajapati@users.noreply.github.com>
2026-08-18 14:44:16 +0530

    conditions practice: grading, logic building exercises

--- logic-building-and-js-questions/script.js
+++ logic-building-and-js-questions/script.js
@@ -1,11 +1,13 @@
 // logics buliding questions and js practice questions
 
+
 // QUESTION 1: var vs let scope
 
 console.log(a);
 var a = 5;
 console.log(b);
 let b = 10;
+
 
 // QUESTION 2. Write a block where var leaks out but let doesn't. Prove it with console.log.
 
@@ -16,6 +18,7 @@ let b = 10;
 console.log(x); // var leaks out of the block, so this will log 5
 console.log(y); // let does not leak out of the block so this will throw a ReferenceError: y is not defined
 
+
 // QUESTION 3. What's the output and why
 
 console.log(1 + "2"); // "12" number 1 is converted to string and concatenated with "2"
@@ -24,6 +27,7 @@ console.log(true + 1); // 2 true is converted to the number 1, then added to 1
 console.log(null + 1); // 1 null is converted to the number 0, then 1 is added
 console.log(undefined + 1); // NaN undefined is converted to the number NaN, then added to 1
 
+
 // QUESTION 4. What's the difference between == and ===? Write one example where they give different outputs.
 
 // == checks for equal value but not type while === checks both value and type.
@@ -31,15 +35,54 @@ console.log(undefined + 1); // NaN undefined is converted to the number NaN, the
 console.log(5 == "5"); // true
 console.log(5 === "5"); // false
 
+
 // QUESTION 5. What does typeof null return and why is it considered a bug?
 
 // typeof null returns "object". This is considered a bug because null is not an object, but a primitive value that represents the absence of any object value.
 
+
 // QUESTION 6. What's the output:
 
-console.log(2 ** 10); // 1024 2 raised to the power of 10
-console.log(10 % 3); // 1 10 divided by 3 leaves a remainder of 1
-console.log(+"42"); // 42 the unary plus operator converts the string "42" to a number
-console.log(+true); // 1 the unary plus operator converts the boolean true to the number 1
-console.log(+false); // 0 the unary plus operator converts the boolean false to the number 0
-console.log(+null); // 0 the unary plus operator converts null to the number 0
+console.log(2 ** 10);  // 1024 2 raised to the power of 10
+console.log(10 % 3);  // 1 10 divided by 3 leaves a remainder of 1
+console.log(+"42");  // 42 the unary plus operator converts the string "42" to a number
+console.log(+true);  // 1 the unary plus operator converts the boolean true to the number 1
+console.log(+false);  // 0 the unary plus operator converts the boolean false to the number 0
+console.log(+null);  // 0 the unary plus operator converts null to the number 0
+
+
+// QUESTION 7. Write a function getGrade(score) that:
+//   - Takes a student's marks (0 to 100) 
+//   - Returns the grade based on this logic:
+//   90-100 A+
+//   80-89 A
+//   70-79 B 
+//   60-69 C 
+//   33-59 D 
+//   0-32 Fail
+//   Anything else Invalid marks
+
+function getGrade(score){
+  if(score<0 || score>100) return "Invalid marks";
+  if(score>=90) return "A+";
+  if(score>=80) return "A";
+  if(score>=70) return "B";
+  if(score>=60) return "C";
+  if(score>=33) return "D";
+  return "Fail";
+}
+console.log(getGrade(2));
+
+
+// QUESTION 8. Rock-Paper-Scissors logic
+
+function getRps(user, computer){
+  if(user !== "Rock" && user !== "Paper" && user !== "Scissor") return "Invalid input";
+  if(computer !== "Rock" && computer !== "Paper" && computer !== "Scissor") return "Invalid input";
+  if(user === "Rock" && computer === "Scissor") return `${user} User won`;
+  if(user === "Paper" && computer === "Rock") return `${user} User won`;
+  if(user === "Scissor" && computer === "Paper") return `${user} User won`;
+  if(user === computer) return "Tied";
+  return `${computer} Computer won`;
+}
+console.log(getRps("Rock", 234));
